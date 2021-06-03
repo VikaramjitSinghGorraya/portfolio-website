f5505a4ca87774946c14c7cba570e643db305244
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-03 15:46:44 -0600

    added Project reducer

---
+++ src/redux/reducers/Project.js
@@ -0,0 +1,41 @@
+import {PROJECTS_FETCHED, PROJECT_POSTED, SUCCESS, FAILURE} from '../Types'
+
+const initialState = {
+    projects:[],
+    success:'',
+    failure:'',
+    isLoading: true
+}
+
+export const projectReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case PROJECTS_FETCHED:
+            return{
+                ...state,
+                projects: action.payload
+            }
+        case PROJECT_POSTED:
+            return{
+                ...state,
+                success: action.payload,
+                error:''
+            }
+        case SUCCESS:
+            return{
+                ...state,
+                success: action.payload,
+                error:''
+            }
+        case FAILURE:
+            return{
+                ...state,
+                error: action.payload,
+                success:'',
+                projects:[]
+            }
+        default:
+            return state
+    }
+}
+
+export default projectReducer
