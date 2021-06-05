882ebf059033d8a715e9f6a02b07cd0c851196e7
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 16:22:03 -0600

    added visitor reducer to root reducer

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import projectReducer from './reducers/Project'
+import visitorReducer from './reducers/Visitor'
 
 const rootReducer = combineReducers({
-    project: projectReducer
+    project: projectReducer,
+    visitor: visitorReducer
 })
 
 export default rootReducer
