1e174adc584ba89660234eae98ca9bade2ed022a
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 13:23:20 -0600

    added redux provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from "@chakra-ui/react"
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import "@fontsource/lato"
 import "@fontsource/oswald"
 import CustomTheme from './styles/CustomTheme'
 import App from './App'
+import store from './redux/Store'
 ReactDOM.render(
     <ChakraProvider theme = {CustomTheme}>
         <BrowserRouter>
+        <Provider store = {store}>
             <App/>
+        </Provider>
         </BrowserRouter>
     </ChakraProvider>,
     document.getElementById('root')
