ea83a706757e460179869cf4774a230ac1037bb2
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 16:21:34 -0600

    added visitor reducer

---
+++ src/redux/reducers/Visitor.js
@@ -0,0 +1,28 @@
+import {SUCCESS, FAILURE} from '../Types'
+
+const initialState = {
+    success:'',
+    failure:'',
+    isLoading: true
+}
+
+export const visitorReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case SUCCESS:
+            return{
+                ...state,
+                success: action.payload,
+                error:''
+            }
+        case FAILURE:
+            return{
+                ...state,
+                error: action.payload,
+                success:'',
+            }
+        default:
+            return state
+    }
+}
+
+export default visitorReducer
