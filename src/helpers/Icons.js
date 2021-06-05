806547c818d07a9de43fcd3330c4b71afd26fc69
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 13:25:20 -0600

    added visit site svg

--- src/helpers/Icons.js
+++ src/helpers/Icons.js
@@ -67,3 +67,9 @@ export const linkedInSvg = () =>{
     return(
         <svg stroke="currentColor" fill="#0077b5" stroke-width="0" viewBox="0 0 448 512" class="contact-module--link--1J7T4" height="1.5em" width="1.5em"  xmlns="http://www.w3.org/2000/svg"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg>   )
 }
+
+export const visitSiteSvg = () =>{
+    return(
+        <svg stroke="currentColor" fill="purple" stroke-width="0" viewBox="0 0 640 512" class="portfolio-module--cloud--2Z7ro" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4z"></path></svg>
+    )
+}
