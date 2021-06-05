f214709b77f6740963e5bc449756dd648841b267
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 13:27:03 -0600

    added information to display

--- src/layout/PortfolioCard.js
+++ src/layout/PortfolioCard.js
@@ -1,21 +1,66 @@
-import React from 'react'
-import {VStack, HStack, Heading, Text, Flex, Image} from '@chakra-ui/react'
-import {githubSvg} from '../helpers/Icons'
+import React, {useEffect, useState} from 'react'
+import {VStack, HStack, Heading, Text, Flex, Image, Link, Box, Skeleton} from '@chakra-ui/react'
+import {motion} from 'framer-motion'
+import { LazyLoadImage, LazyLoadComponent } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
+import 'react-lazy-load-image-component/src/effects/black-and-white.css'
+import {githubSvg, visitSiteSvg} from '../helpers/Icons'
 import image from '../image/ds.png'
+import axios from 'axios'
 
-const PortfolioCard = () => {
+const MotionVStack = motion(VStack)
+const PortfolioCard = ({ photo ,projectId, title, description, technologiesUsed, githubLink, liveLink}) => {
+    
+    const [projectImage, setProjectImage] = useState(null)
+    
+    const fetchImage = () =>{
+        axios.get(`https://portfolio-website-backend.vercel.app/api/${projectId}/getProjectPhoto`)
+        .then((res)=> {setProjectImage(res.config.url)
+        })
+        .catch(err=>console.log(err))
+    }
+
+    useEffect(()=>{
+        fetchImage()
+    },[])
+
     return (
-            <VStack alignItems = 'flex-start' spacing = {6} maxW = '45%' mt = '10'>
-                <Image src= {image} objectFit = 'cover'/>
-                <Heading>Algorithm Tests</Heading>
-                    <VStack alignItems = 'flex-start'>
-                        <Text as = 'p' fontSize = 'xl'>A collection of toy problems I have solved with their accompanying Jest tests. I thought the simple well defined inputs and outputs of problems on websites like L33tCode were an excellent opportunity to learn how to write better tests in Jest.  </Text>
-                        <HStack>
-                            {githubSvg()}
-                            <Text>Github</Text>
+            <MotionVStack initial ={{marginTop:100, opacity:0}} animate = {{marginTop:0, opacity:1}} transition = {{duration:0.4}} alignItems = 'flex-start' spacing = {6} flex= {[1,1,0.49,0.49]} mb = '10'>
+                <Box minH = '16em' minW = '100%'>
+                    <LazyLoadImage
+                        effect = 'blur'
+                        height= '100%'
+                        src={projectImage} 
+                        alt = 'image'
+                        width='100%'
+                    />
+                </Box>
+                <LazyLoadComponent >
+                <Heading as = 'h6' fontSize = '2xl'>{title}</Heading>
+                <VStack alignItems = 'flex-start' spacing = {5}>
+                    <Text as = 'p' fontSize = 'xl'>{description} </Text>
+                    <VStack alignItems = 'flex-start' spacing = {0}>
+                        <Text fontWeight = 'bold'>Tech used:</Text>
+                        <HStack flexWrap = 'wrap' spacing = {0}>
+                            {technologiesUsed.map((technology, index)=>(
+                                    <Text key = {index} as = 'p' fontWeight = 'bold'>{technology}{index===technologiesUsed.length-1? '' : `,\u00A0 \u00A0`} </Text>
+                            ))}
                         </HStack>
                     </VStack>
-            </VStack>
+                    <HStack spacing = {10}>
+                        <HStack>
+                            {githubSvg()}
+                            <Link isExternal href = {githubLink}>Github</Link>
+                        </HStack>
+                        <HStack>
+                            {visitSiteSvg()}
+                            <Link isExternal href = {liveLink}>Visit the website</Link>
+                        </HStack>
+                    </HStack>
+                </VStack>
+                </LazyLoadComponent>
+                
+            </MotionVStack>
 )}
 
 export default PortfolioCard
