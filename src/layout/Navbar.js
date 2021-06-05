a894f7c142009a94f800bcc20b1c5cba2456c88b
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-05 13:24:44 -0600

    added motion animation

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { HStack, Link } from "@chakra-ui/react"
 import { useHistory } from 'react-router'
+import {motion} from 'framer-motion'
+const MotionLink = motion(Link)
 const Navbar = () => {
     
     var history = useHistory()
 
     return (
         <HStack spacing = {5} minH = '100'>
-            <Link href = '/' borderBottom = {history.location.pathname === '/'? '2px solid green': ''} fontWeight = "extrabold" fontSize = "20">Vikaramjit Singh</Link>
-            <Link href = '/About' borderBottom = {history.location.pathname === '/About'? '2px solid green': ''}  fontWeight = {history.location.pathname === '/About'?  "bold": 'normal'}  _selected = {{fontWeight: 'bold'}}>About</Link>
-            <Link href = '/Contact' borderBottom = {history.location.pathname === '/Contact'? '2px solid green': ''} fontWeight = {history.location.pathname === '/Contact'?  "bold": 'normal'}>Contact</Link>
+            <MotionLink background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/' fontWeight = "extrabold" fontSize = "20">Vikaramjit Singh</MotionLink>
+            <MotionLink  background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/About'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/About' fontWeight = {history.location.pathname === '/About'?  "bold": 'normal'}>About</MotionLink>
+            <MotionLink background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/Contact'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/Contact'  fontWeight = {history.location.pathname === '/Contact'?  "bold": 'normal'}>Contact</MotionLink>
         </HStack>
     )
 }
