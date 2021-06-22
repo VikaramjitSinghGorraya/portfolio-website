d933c246cd85962b744c3c10ae05876a93cafe8a
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-22 17:22:17 -0600

    changed icon margin

--- src/helpers/Icons.js
+++ src/helpers/Icons.js
@@ -76,6 +76,6 @@ export const visitSiteSvg = () =>{
 
 export const resumeDownloadSvg = () =>{
     return(
-        <svg xmlns="http://www.w3.org/2000/svg" style = {{marginRight: '2em'}} width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icns"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" style = {{marginRight: '20px'}} width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icns"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
     )
 }
