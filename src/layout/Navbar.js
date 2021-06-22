26e702421db4c62512380b705b6cf8f80a97a46b
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-22 15:54:09 -0600

    added resume tab to navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = () => {
             <MotionLink background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/' fontWeight = "extrabold" fontSize = {["15","20"]}>Vikaramjit Singh</MotionLink>
             <MotionLink  background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/About'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/About' fontWeight = {history.location.pathname === '/About'?  "bold": 'normal'}>About</MotionLink>
             <MotionLink background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/Contact'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/Contact'  fontWeight = {history.location.pathname === '/Contact'?  "bold": 'normal'}>Contact</MotionLink>
+            <MotionLink background= 'linear-gradient(green, green) bottom/0% 2px no-repeat' initial = {{backgroundSize:'none'}} animate ={history.location.pathname === '/Resume'? { backgroundSize: '100% 2px'}:''} transition = {{duration:0.25}} href = '/Resume'  fontWeight = {history.location.pathname === '/Resumet'?  "bold": 'normal'}>Resume</MotionLink>
         </HStack>
     )
 }
