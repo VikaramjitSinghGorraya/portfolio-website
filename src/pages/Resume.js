9c4fec528cd284cdf18e6d42822d31df90ca424d
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-22 16:22:19 -0600

    adjusted height of resume image

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -15,15 +15,17 @@ const Resume = () => {
     }
 
     return (
-        <MotionVStack initial = {{x:-100, opacity:0}} animate = {{x:0, opacity:1}} transition = {{duration:0.2}} exit = {{x:100, opacity:1}} alignItems = 'center' spacing = {0} overflow = 'hidden' pb = '10' mt = {['auto','50']}>
-            <Header heading = 'My Resume'/>
-            <Box minH = '80vh'>
-                {resumeDisplay()}
-            </Box>
-            <Button w = '40%' h ='3em' m = 'auto' onClick = {()=>window.location = 'Final Resume.pdf'}>
-                {resumeDownloadSvg()}
-                DOWNLOAD RESUME.pdf
-            </Button>
+        <MotionVStack initial = {{x:-100, opacity:0}} animate = {{x:0, opacity:1}} transition = {{duration:0.2}} exit = {{x:100, opacity:1}} alignItems = 'center' spacing = {20} overflow = 'hidden' pb = '10' mt = {['auto','50']}>
+            <Header heading = 'Resume'/>
+            <VStack spacing = {5}>
+                <Box minH = '60vh'>
+                    {resumeDisplay()}
+                </Box>
+                <Button w = {['100%','40%']} h ='3em' m = 'auto' onClick = {()=>window.open('Final Resume.pdf', '_blank')}>
+                    {resumeDownloadSvg()}
+                    DOWNLOAD RESUME .PDF
+                </Button>
+            </VStack>
         </MotionVStack>
     )
 }
