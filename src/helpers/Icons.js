65f4d841364c43dff382f4351f19d68e65b496b9
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-22 15:55:40 -0600

    added resumeDownload svg

--- src/helpers/Icons.js
+++ src/helpers/Icons.js
@@ -73,3 +73,9 @@ export const visitSiteSvg = () =>{
         <svg stroke="currentColor" fill="purple" stroke-width="0" viewBox="0 0 640 512" class="portfolio-module--cloud--2Z7ro" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4z"></path></svg>
     )
 }
+
+export const resumeDownloadSvg = () =>{
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" style = {{marginRight: '2em'}} width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icns"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+    )
+}
