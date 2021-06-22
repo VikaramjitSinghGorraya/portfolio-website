278b932b9f33a72c80ad8cd7c847e3d62f408cd5
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-06-22 16:26:12 -0600

    reduce length of resume image

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -18,7 +18,7 @@ const Resume = () => {
         <MotionVStack initial = {{x:-100, opacity:0}} animate = {{x:0, opacity:1}} transition = {{duration:0.2}} exit = {{x:100, opacity:1}} alignItems = 'center' spacing = {20} overflow = 'hidden' pb = '10' mt = {['auto','50']}>
             <Header heading = 'Resume'/>
             <VStack spacing = {5}>
-                <Box minH = '60vh'>
+                <Box minH = '60vh' w = '90%' mx = 'auto'>
                     {resumeDisplay()}
                 </Box>
                 <Button w = {['100%','40%']} h ='3em' m = 'auto' onClick = {()=>window.open('Final Resume.pdf', '_blank')}>
